0589869497a53b74de1eca9ec9936c9483a3c6e6
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-29 09:43:45 +0100

    install laravel

---
+++ resources/views/register.blade.php
@@ -0,0 +1,11 @@
+<div>
+
+<form action="" method="post">
+
+    <input type="text" name="username" id="" placeholder="username">
+    <input type="password" name="password" id="" placeholder="password">
+    <input type="submit" name="register" id="" >
+
+</form>
+
+</div>
