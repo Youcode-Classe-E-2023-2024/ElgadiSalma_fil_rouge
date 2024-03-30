a55e394dce812fcefcc386244f7473d61a07c71e
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-30 13:32:57 +0000

    create_capitale_table

---
+++ database/migrations/2024_03_30_125709_create_capitale.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capitale', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('number');
+            $table->bigInteger('pharmacie_id');
+            $table->timestamps();
+
+            $table->foreign('pharmacie_id')->references('id')->on('pharmacie')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('capitale');
+    }
+};
