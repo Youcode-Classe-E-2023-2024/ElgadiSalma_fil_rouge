6fcbdff6b6e844af779873e0f0fc49e10e09942a
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-01 18:51:41 +0000

    crud medicine

---
+++ backPharma/app/Http/Controllers/medicamentController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medicine;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
+class medicamentController extends Controller
+{
+    public function addMedicine(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'category' => 'required',
+            'expiration' => 'required|date',
+            'image' =>'required',
+        ]);
+
+        // if ($validator->fails()) {
+        //     return back()->withErrors($validator)->withInput();
+        // }
+
+        $medicine = Medicine::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'image' => $request->input('image'),
+            'category_id' => $request->input('category'),
+            'expiration' => $request->input('expiration'),
+            // 'created_by' => $user->id,
+        ]);
+
+        
+        return response()->json(['message' => 'medicine added successfully', 'medicine' => $medicine], 201);
+    }
+
+    public function editMedicine(Request $request, $id)
+    {
+        $medicine = Medicine::find($id);
+
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'category' => 'required',
+            'expiration' => 'required|date',
+            'image' =>'required',
+        ]);
+
+        if (!$medicine) {
+            return response()->json(['message' => 'Medicine not found'], 404);
+        }
+
+        // if ($validator->fails()) {
+        //     return back()->withErrors($validator)->withInput();
+        // }
+
+        $medicine-> name = $request->input('name');
+        $medicine-> description = $request->input('description');
+        $medicine-> price = $request->input('price');
+        $medicine-> image = $request->input('image');
+        $medicine-> category_id = $request->input('category');
+        $medicine-> expiration = $request->input('expiration');
+
+        $medicine->save();
+
+        
+        return response()->json(['message' => 'medicine updated successfully', 'medicine' => $medicine], 201);
+    }
+
+    public function deleteMedicine($id)
+    {
+        $medicine = Medicine::find($id);
+        $medicine->delete();
+
+        if (!$medicine) {
+            return response()->json(['message' => 'medicine not found'], 404);
+        }
+        return response()->json(['message' => 'medicine deleted successfully'], 200);
+    }
+
+}
--- backPharma/routes/api.php
+++ backPharma/routes/api.php
@@ -4,15 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\medicamentController;
 
 /*
 |--------------------------------------------------------------------------
 | Authentification
 |--------------------------------------------------------------------------
 */
+
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +22,14 @@ Route::post('logout', [AuthController::class, 'logout']);
 Route::post('/forgotPassword', [AuthController::class, 'forgotPassword']);
 Route::get('/reset/{token}', [AuthController::class, 'showResetForm'])->name('password.reset');
 Route::post('/resetPassword', [AuthController::class, 'reset']);
-    
+
 
 Route::group([
     'middleware' => 'auth:api'
 ], function () {
     Route::get('helloworld', [AuthController::class, 'index']);
-
-
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('/medicine', [medicamentController::class, 'addMedicine']);
+    Route::put('/medicine/{id}', [medicamentController::class, 'editMedicine']);
+    Route::delete('/medicine/{id}', [medicamentController::class, 'deleteMedicine']);
 });
