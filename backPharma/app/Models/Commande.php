ca5d84febe32eb0844a01d65e2a5e26572c5cca5
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-05 09:53:32 +0000

    commande model

---
+++ backPharma/app/Models/Commande.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commande extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'pharmacie_id',
+        'medicament_id',
+        'number',
+        'dateDepart',
+        'dateArrive',
+        'requested_by',
+        'accepted',
+    ];
+
+    protected $table = 'commande';
+}
