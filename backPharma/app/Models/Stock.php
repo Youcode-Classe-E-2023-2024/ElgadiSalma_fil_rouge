75acf26353722419830a926a7558758d35ca232d
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-08 09:46:38 +0000

    Stock model

---
+++ backPharma/app/Models/Stock.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'logo',
+        'city_id',
+        'adresse',
+        'created_by',
+    ];
+
+    protected $table = 'pharmacie';
+}
