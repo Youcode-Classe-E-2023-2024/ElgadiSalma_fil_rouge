763217892bd14022d1739f1d19d6e106676e0ad0
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-13 01:26:24 +0000

    decrementer le capitale lors de l'ajout d'un stock

--- backPharma/app/Console/Kernel.php
+++ backPharma/app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Models\Stock;
+use App\Models\Capitale;
 use App\Models\Commande;
 use App\Models\Medicine;
 use Illuminate\Console\Scheduling\Schedule;
@@ -23,12 +24,23 @@ class Kernel extends ConsoleKernel
                 $medicine = Medicine::find($commande->medicament_id);
                 $prixTotal = $medicine->price * $commande->number;
     
-                Stock::create([
+                $stock = Stock::create([
                     'medicament_id' => $commande->medicament_id,
                     'pharmacie_id' => $commande->pharmacie_id,
                     'number' => $commande->number,
                     'price' => $prixTotal,
                 ]);
+
+                if ($stock) {
+                    $editedCapital = Capitale::where('pharmacie_id', $commande->pharmacie_id)->first();
+
+                    if ($editedCapital) 
+                    {
+                        $editedCapital->number = $editedCapital->number - $prixTotal;
+                        $editedCapital->save();
+                    }
+                }
+        
     
             }})->everyMinute();
     }
