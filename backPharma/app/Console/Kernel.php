815801e46f6ddbce40a8751581c6f5464e1d42e0
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-16 11:48:53 +0100

    add to stock automatically

--- backPharma/app/Console/Kernel.php
+++ backPharma/app/Console/Kernel.php
@@ -18,15 +18,19 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
-            $pendingCommands = Commande::where('dateArrive', '<=', now())->where('accepted', true)->get();
+            $pendingCommands = Commande::where('dateArrive', '<=', now())->where('accepted', true)->where('addedToStock', false)->get();
 
             foreach ($pendingCommands as $commande) {
+                $commande->addedToStock = true;
+                $commande->save();
+
                 $medicine = Medicine::find($commande->medicament_id);
                 $prixTotal = $medicine->price * $commande->number;
     
                 $stock = Stock::create([
                     'medicament_id' => $commande->medicament_id,
                     'pharmacie_id' => $commande->pharmacie_id,
+                    'initialNumber' => $commande->number,
                     'number' => $commande->number,
                     'price' => $prixTotal,
                 ]);
