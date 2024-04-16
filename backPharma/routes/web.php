d9ff768dc792759d39d14152af84d410bd8cc46e
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-16 11:45:40 +0100

    display pages front

--- backPharma/routes/web.php
+++ backPharma/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('Guest.homePage');
+});
+
+Route::get('/medicament', function () {
+    return view('Guest.medicineList');
+});
+
+Route::get('/about-us', function () {
+    return view('Guest.about-us');
+});
+
+Route::get('/login', function () {
+    return view('Authentification.login');
+});
+
+Route::get('/forgot-password', function () {
+    return view('Authentification.forgot-password');
+});
