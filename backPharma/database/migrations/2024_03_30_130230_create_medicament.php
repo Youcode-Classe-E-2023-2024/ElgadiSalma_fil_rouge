4b9454561506eef831bb00c1e9fc7f9e392a7ba2
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-21 12:29:08 +0100

    update medicament table

--- backPharma/database/migrations/2024_03_30_130230_create_medicament.php
+++ backPharma/database/migrations/2024_03_30_130230_create_medicament.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('medicament', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('image');
             $table->string('expiration');
             $table->bigInteger('price');
