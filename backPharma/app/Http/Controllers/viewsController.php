e9598649cd7f7898096bdd84f25829e661000023
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-21 12:31:27 +0100

    Update viewsController.php

--- backPharma/app/Http/Controllers/viewsController.php
+++ backPharma/app/Http/Controllers/viewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Role;
 use App\Models\Category;
 use App\Models\Medicine;
@@ -16,6 +17,27 @@ class viewsController extends Controller
     {
         $medicines = Medicine::orderBy('created_at', 'desc')->get();
         return view('Guest.medicineList', ['medicines' => $medicines]);
+    }
+
+    public function medicineUser()
+    {
+        
+        $medicines = Medicine::orderBy('created_at', 'desc')->get();
+        $me = Auth::user();
+        $role = Role::find($me->role_id);
+        $data = [
+            'totalMedicines' => Medicine::count(),
+            'totalCategories' => Category::count(),
+            'totalPharmacies' => Pharmacie::count(),
+            'totalUsers' => User::count(),
+        ];
+
+
+        return view('Moderateur.medicineUser', [
+            'me' => $me,
+            'role' => $role,
+            'medicines' => $medicines
+        ]);
     }
 
     public function adminDashboard()
@@ -35,6 +57,34 @@ class viewsController extends Controller
 
 
         return view('Admin.adminDashboard', [
+            'me' => $me,
+            'role' => $role,
+            'data' => $data,
+            'categories' => $categories,
+            'Users' => $Users,
+            'Pharmacies' => $Pharmacies,
+            'Medicines' => $Medicines
+        ]);
+    }
+
+
+    public function moderateurDashboard()
+    {
+        $categories = Category::paginate(2, ['*'], 'category');
+        $Users = User::paginate(2, ['*'], 'users');
+        $Pharmacies = Pharmacie::paginate(2, ['*'], 'pharmacies');
+        $Medicines = Medicine::all();
+        $me = Auth::user();
+        $role = Role::find($me->role_id);
+        $data = [
+            'totalMedicines' => Medicine::count(),
+            'totalCategories' => Category::count(),
+            'totalPharmacies' => Pharmacie::count(),
+            'totalUsers' => User::count(),
+        ];
+
+
+        return view('Moderateur.moderateurDash', [
             'me' => $me,
             'role' => $role,
             'data' => $data,
@@ -67,4 +117,52 @@ class viewsController extends Controller
         $role = Role::find($me->role_id);
         return view('Admin.addCategory', ['me' => $me, 'role' => $role]);
     }
+
+    public function venteView()
+    {
+        $medicines = Medicine::with('category')->orderBy('created_at', 'desc')->get();
+        $me = Auth::user();
+        $role = Role::find($me->role_id);
+        $data = [
+            'totalMedicines' => Medicine::count(),
+            'totalCategories' => Category::count(),
+            'totalPharmacies' => Pharmacie::count(),
+            'totalUsers' => User::count(),
+        ];
+        // $categories = Category::all();
+
+        return view('Moderateur.medicineUser', [
+            'me' => $me,
+            'role' => $role,
+            'medicines' => $medicines
+        ]);
+    }
+
+    public function pharmaAddView()
+    {
+        $cities = City::all();
+
+        return view('Moderateur.addPharma' , ['cities' => $cities]);
+    }
+
+    public function addCommandeView()
+    {
+        $medicines = Medicine::with('category')->orderBy('created_at', 'desc')->get();
+        $me = Auth::user();
+        $role = Role::find($me->role_id);
+        $data = [
+            'totalMedicines' => Medicine::count(),
+            'totalCategories' => Category::count(),
+            'totalPharmacies' => Pharmacie::count(),
+            'totalUsers' => User::count(),
+        ];
+        // $categories = Category::all();
+
+        return view('Moderateur.addCommande', [
+            'me' => $me,
+            'role' => $role,
+            'medicines' => $medicines
+        ]);
+    }
+    
 }
