4f4569fc2f64e7f417bfd9ac5e5beafc43d0eadf
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-23 15:54:33 +0100

    Update medicineList.blade.php

--- backPharma/resources/views/Guest/medicineList.blade.php
+++ backPharma/resources/views/Guest/medicineList.blade.php
@@ -1,96 +1,99 @@
 @extends('Guest.guestLayout')
 @section('guestContent')
+    <!------main-content------>
+    <main class="main-content">
 
-<!------main-content------>
-<main class="main-content">
-          
 
-    <!-----------------breadcrumb------------------------>
-    <section class="ban-bread-crumb">
+        <!-----------------breadcrumb------------------------>
+        <section class="ban-bread-crumb">
             <div class="container">
-               <div class="row">
-                  <div class="col-lg-12">
-                     <nav aria-label="breadcrumb">
-               <ol class="breadcrumb">
-                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                 <li class="breadcrumb-item"><a href="#">Department</a></li>
-                 <li class="breadcrumb-item active" aria-current="page"> Grid 3 Column</li>
-               </ol>
-         </nav>
-                  </div>
-               </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="#">Department</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"> Grid 3 Column</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
             </div>
-         
-         </section>
-         
-    <!-----------------breadcrumb------------------------>
-    
-    
-       <!-----------------OUR DEPARTMENTS 3 Column------------------------>
-         <div class="department_gdthre_column">
+
+        </section>
+
+        <!-----------------breadcrumb------------------------>
+
+
+        <!-----------------OUR DEPARTMENTS 3 Column------------------------>
+        <div class="department_gdthre_column">
             <div class="container">
-               <div class="row">
-                  <div class="col-lg-12">
-                      <div class="heading">
-                          <h1>OUR <span>DEPARTMENTS</span></h1>
-                      </div>
-                  </div>
-               </div>
-               <div class="row">
-                  @foreach ($medicines as $medicine)
-                  <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                      <div class="department_gd_inner">
-                         <div class="image">
-                            <img src="assets/image/departments/department-1.jpg" class="img-fluid" alt="img" />
-                         </div>
-                         <div class="dp_content">
-                            <h2><a href="#">{{$medicine->name}}</a></h2>
-                            <p>{{$medicine->price}} DH</p>
-                            <a href="#"  class="read_" >Read more</a>
-                         </div>
-                      </div>
-                  </div>
-                  @endforeach
-               </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="heading">
+                            <h1>OUR <span>DEPARTMENTS</span></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    @foreach ($medicines as $medicine)
+                        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                            <div class="department_gd_inner">
+                                <div class="image">
+                                    <img src="assets/image/departments/department-1.jpg" class="img-fluid" alt="img" />
+                                </div>
+                                <div class="dp_content">
+                                    <h2><a href="#">{{ $medicine->name }}</a></h2>
+                                    <p>{{ $medicine->price }} DH</p>
+                                    @foreach ($stockTotals as $stockTotal)
+                                        <h1>{{ $stockTotal['total_number'] }}</h1>
+                                    @endforeach
+                                    <a href="#" class="read_">Read more</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-         </div>
-       <!-----------------OUR DEPARTMENTS 3 Column------------------------>  
-    
-    
-    <!-------contact-through----->
-    <div class="contact_through">
-          <div class="container">
-             <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                   <div class="contact_through_inner">
-                      <span class="flaticon-phone-1 icon"></span>
-                      <p>Emergency Cases: <br/>
-                         (052) 611-5711</p>
-                   </div>
+        </div>
+
+        <!-----------------OUR DEPARTMENTS 3 Column------------------------>
+
+
+        <!-------contact-through----->
+        <div class="contact_through">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="contact_through_inner">
+                            <span class="flaticon-phone-1 icon"></span>
+                            <p>Emergency Cases: <br />
+                                (052) 611-5711</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="contact_through_inner">
+                            <span class="flaticon-placeholder icon"></span>
+                            <p>E7088 Micaela Cliffs, <br />
+                                Thielshire, OK 95062</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="contact_through_inner">
+                            <span class="flaticon-envelope icon"></span>
+                            <p>Email Address<br />
+                                contact@meditex.com</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="contact_through_inner">
+                            <span class="flaticon-calendar icon"></span>
+                            <p>Book Online<br />
+                                Appointment Now</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                   <div class="contact_through_inner">
-                      <span class="flaticon-placeholder icon"></span>
-                      <p>E7088 Micaela Cliffs, <br/>
-                         Thielshire, OK 95062</p>
-                   </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                   <div class="contact_through_inner">
-                      <span class="flaticon-envelope icon"></span>
-                      <p>Email Address<br/>
-                         contact@meditex.com</p>
-                   </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                   <div class="contact_through_inner">
-                      <span class="flaticon-calendar icon"></span>
-                      <p>Book Online<br/>
-                         Appointment Now</p>
-                   </div>
-                </div>
-             </div>
-          </div>
-       </div>
-       <!-------contact-through----->
-@endsection    
+            </div>
+        </div>
+        <!-------contact-through----->
+    @endsection
