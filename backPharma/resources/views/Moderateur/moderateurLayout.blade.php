681b405b235da8196c4d9207fb65e903e825f077
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-23 15:48:30 +0100

    btn submit front

--- backPharma/resources/views/Moderateur/moderateurLayout.blade.php
+++ backPharma/resources/views/Moderateur/moderateurLayout.blade.php
@@ -31,6 +31,71 @@
     <script src="https://rawgit.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js"></script>
 
     <style>
+        .buttonEdit {
+            width: 4rem;
+            padding: 0;
+            border: none;
+            transform: rotate(5deg);
+            transform-origin: center;
+            font-family: "Gochi Hand", cursive;
+            text-decoration: none;
+            font-size: 9px;
+            cursor: pointer;
+            padding-bottom: 3px;
+            border-radius: 5px;
+            box-shadow: 0 2px 0 #494a4b;
+            tranFsition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+            background-color: #5cdb95;
+        }
+
+        .buttonEdit span {
+            background: #f1f5f8;
+            display: block;
+            padding: 0.1rem 0.5rem;
+            border-radius: 5px;
+            border: 2px solid #494a4b;
+        }
+
+        .buttonEdit:active {
+            transform: translateY(5px);
+            padding-bottom: 0px;
+            outline: 0;
+        }
+
+        .buttonDelete {
+            width: 4rem;
+            padding: 0;
+            border: none;
+            transform: rotate(5deg);
+            transform-origin: center;
+            font-family: "Gochi Hand", cursive;
+            text-decoration: none;
+            font-size: 9px;
+            cursor: pointer;
+            padding-bottom: 3px;
+            border-radius: 5px;
+            box-shadow: 0 2px 0 #494a4b;
+            tranFsition: all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+            background-color: #db715c;
+        }
+
+        .buttonDelete span {
+            background: #f1f5f8;
+            display: block;
+            padding: 0.1rem 0.5rem;
+            border-radius: 5px;
+            border: 2px solid #494a4b;
+        }
+
+        .buttonDelete:active {
+            transform: translateY(5px);
+            padding-bottom: 0px;
+            outline: 0;
+        }
+
+
+
+
         .card {
             overflow: visible;
             width: 17rem;
