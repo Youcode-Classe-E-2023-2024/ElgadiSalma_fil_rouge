a111cf3359a0508c39a45203fd3d6707ba25eabf
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-24 15:04:50 +0100

    one to many relation

--- backPharma/app/Models/Pharmacie.php
+++ backPharma/app/Models/Pharmacie.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\City;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pharmacie extends Model
 {
@@ -19,4 +20,9 @@ class Pharmacie extends Model
 
     protected $table = 'pharmacie';
 
+    public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id');
+    }
+   
 }
