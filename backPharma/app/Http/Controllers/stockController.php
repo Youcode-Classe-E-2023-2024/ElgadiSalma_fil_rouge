78bb4ef596af039f1852da35bc801682d8b6323b
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-25 22:05:29 +0100

    add commande to stock function

--- backPharma/app/Http/Controllers/stockController.php
+++ backPharma/app/Http/Controllers/stockController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stock;
+use App\Models\Capitale;
+use App\Models\Commande;
+use App\Models\Medicine;
 use Illuminate\Http\Request;
 
 class stockController extends Controller
 {
-    //
+    public function addToStock()
+    {
+        $pendingCommands = Commande::where('dateArrive', '<=', now())->where('accepted', true)->where('addedToStock', false)->get();
+
+        foreach ($pendingCommands as $commande) {
+            $commande->addedToStock = true;
+            $commande->save();
+
+            $medicine = Medicine::find($commande->medicament_id);
+            $prixTotal = $medicine->price * $commande->number;
+
+            $stock = Stock::create([
+                'medicament_id' => $commande->medicament_id,
+                'pharmacie_id' => $commande->pharmacie_id,
+                'initialNumber' => $commande->number,
+                'number' => $commande->number,
+                'price' => $prixTotal,
+            ]);
+
+            if ($stock) {
+                $editedCapital = Capitale::where('pharmacie_id', $commande->pharmacie_id)->first();
+
+                if ($editedCapital) {
+                    $editedCapital->number = $editedCapital->number - $prixTotal;
+                    $editedCapital->save();
+                }
+            }
+        }
+
+        return redirect()->back()->with('success', "Médicament non trouvé");
+    }
 }
