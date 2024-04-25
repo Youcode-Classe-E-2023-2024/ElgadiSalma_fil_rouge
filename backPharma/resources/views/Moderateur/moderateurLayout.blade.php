454c8e79d5cc2a3a7804823924c07645d6ace4be
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-25 22:03:28 +0100

    front updates

--- backPharma/resources/views/Moderateur/moderateurLayout.blade.php
+++ backPharma/resources/views/Moderateur/moderateurLayout.blade.php
@@ -31,6 +31,177 @@
     <script src="https://rawgit.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.js"></script>
 
     <style>
+        .botonaa {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 13rem;
+            overflow: hidden;
+            height: 3rem;
+            background-size: 300% 300%;
+            backdrop-filter: blur(1rem);
+            border-radius: 5rem;
+            transition: 0.5s;
+            animation: gradient_301 5s ease infinite;
+            border: double 4px transparent;
+            background-image: linear-gradient(#212121, #212121), linear-gradient(137.48deg, #0d4436 10%, #bbb6e1 45%, #287571 67%, #0a3a3b 87%);
+            background-origin: border-box;
+            background-clip: content-box, border-box;
+        }
+
+        #container-stars {
+            position: absolute;
+            z-index: -1;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            transition: 0.5s;
+            backdrop-filter: blur(1rem);
+            border-radius: 5rem;
+        }
+
+        strong {
+            z-index: 2;
+            font-family: 'Avalors Personal Use';
+            font-size: 12px;
+            letter-spacing: 5px;
+            color: #FFFFFF;
+            text-shadow: 0 0 4px white;
+        }
+
+        #glow {
+            position: absolute;
+            display: flex;
+            width: 12rem;
+        }
+
+        .circle {
+            width: 100%;
+            height: 30px;
+            filter: blur(2rem);
+            animation: pulse_3011 4s infinite;
+            z-index: -1;
+        }
+
+        .circle:nth-of-type(1) {
+            background: rgba(78, 215, 188, 0.636);
+        }
+
+        .circle:nth-of-type(2) {
+            background: rgba(132, 155, 193, 0.704);
+        }
+
+        .botonaa:hover #container-stars {
+            z-index: 1;
+            background-color: #212121;
+        }
+
+        .botonaa:hover {
+            transform: scale(1.1)
+        }
+
+        .botonaa:active {
+            border: double 4px #15584f;
+            background-origin: border-box;
+            background-clip: content-box, border-box;
+            animation: none;
+        }
+
+        .botonaa:active .circle {
+            background: #53f0fe;
+        }
+
+        #stars {
+            position: relative;
+            background: transparent;
+            width: 200rem;
+            height: 200rem;
+        }
+
+        #stars::after {
+            content: "";
+            position: absolute;
+            top: -10rem;
+            left: -100rem;
+            width: 100%;
+            height: 100%;
+            animation: animStarRotate 90s linear infinite;
+        }
+
+        #stars::after {
+            background-image: radial-gradient(#ffffff 1px, transparent 1%);
+            background-size: 50px 50px;
+        }
+
+        #stars::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: -50%;
+            width: 170%;
+            height: 500%;
+            animation: animStar 60s linear infinite;
+        }
+
+        #stars::before {
+            background-image: radial-gradient(#ffffff 1px, transparent 1%);
+            background-size: 50px 50px;
+            opacity: 0.5;
+        }
+
+        @keyframes animStar {
+            from {
+                transform: translateY(0);
+            }
+
+            to {
+                transform: translateY(-135rem);
+            }
+        }
+
+        @keyframes animStarRotate {
+            from {
+                transform: rotate(360deg);
+            }
+
+            to {
+                transform: rotate(0);
+            }
+        }
+
+        @keyframes gradient_301 {
+            0% {
+                background-position: 0% 50%;
+            }
+
+            50% {
+                background-position: 100% 50%;
+            }
+
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+
+        @keyframes pulse_3011 {
+            0% {
+                transform: scale(0.75);
+                box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.7);
+            }
+
+            70% {
+                transform: scale(1);
+                box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+            }
+
+            100% {
+                transform: scale(0.75);
+                box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+            }
+        }
+
+
+
         .SubmitAddMedicine {
             position: relative;
             margin: 1rem 10rem;
@@ -382,13 +553,9 @@
         <header class="main-header">
             <!-- Logo -->
             <a href="index.html" class="logo blue-bg">
-                <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><img src="assets-dashboard-utilisateur/img/logo-n.png" alt=""></span>
-                <!-- logo for regular state and mobile devices -->
                 <span class="logo-lg"><img src="assets-dashboard-utilisateur/img/logo.png" alt=""></span> </a>
-            <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar blue-bg navbar-static-top">
-                <!-- Sidebar toggle button-->
                 <ul class="nav navbar-nav pull-left">
                     <li><a class="sidebar-toggle" data-toggle="push-menu" href=""></a> </li>
                 </ul>
@@ -402,12 +569,10 @@
                             </span>
                         </div>
                     </form>
-                    <!-- search form -->
                 </div>
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
 
-                        <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu p-ph-res"> <a href="#" class="dropdown-toggle"
                                 data-toggle="dropdown"> <img src="{{ asset('storage/images/users/' . $me->photo) }}"
                                     class="user-image" alt="User Image"> <span
@@ -463,17 +628,17 @@
 
                         </ul>
                     </li>
-                    
+
                     <li class="treeview"> <a href="#"> <i class="fa fa-briefcase"></i> <span>Formulaires</span>
                             <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span>
                         </a>
                         <ul class="treeview-menu">
                             <li><a href="/addUser">AddUser</a></li>
                             <li><a href="/commande">addCommande</a></li>
-                            
+
                         </ul>
                     </li>
-                    
+
                 </ul>
             </div>
             <!-- /.sidebar -->
@@ -482,13 +647,7 @@
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
-            <div class="content-header sty-one">
-                <h1>Dashboard 4</h1>
-                <ol class="breadcrumb">
-                    <li><a href="#">Home</a></li>
-                    <li><i class="fa fa-angle-right"></i> Dashboard 4</li>
-                </ol>
-            </div>
+
             <!-- /.content-wrapper -->
 
 
