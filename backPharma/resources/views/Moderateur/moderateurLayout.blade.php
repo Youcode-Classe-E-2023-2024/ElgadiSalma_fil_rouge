fe7888fbf169f5963db83bf2a3e78b125bfead84
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-25 09:57:40 +0100

    Update moderateurLayout.blade.php

--- backPharma/resources/views/Moderateur/moderateurLayout.blade.php
+++ backPharma/resources/views/Moderateur/moderateurLayout.blade.php
@@ -459,7 +459,6 @@
                     <li class="treeview"> <a href="#"> <i class="fa fa-bullseye"></i> <span>Apps</span> <span
                                 class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
                         <ul class="treeview-menu">
-                            <li><a href="/approuvements">Approuvements</a></li>
                             <li><a href="/vente" class="active">Vente</a></li>
 
                         </ul>
